53635e89a5e58b782de1390ea354d96f9bae0edf
dyachan <diego.yachan@gmail.com>
2013-09-30 09:56:52 -0300

    corregido errores varios

--- getAgenda.sh
+++ getAgenda.sh
@@ -1,1 +1,1 @@
-gcalcli --user diego.yachan@gmail.com --pw lamparas --nc --details agenda $(date +"%m/%d/%Y") $(date -d "30 days" +"%m/%d/%Y")
+gcalcli --user usuario@gmail.com --pw clave --nc --details agenda $(date +"%m/%d/%Y") $(date -d "30 days" +"%m/%d/%Y")
