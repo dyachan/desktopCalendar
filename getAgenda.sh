442078c587ab6a7af7e6c76f8df3da2c109b5944
dyachan <diego.yachan@gmail.com>
2013-10-03 13:59:54 -0300

    corregidas lineas de ejemplo

--- getAgenda.sh
+++ getAgenda.sh
@@ -1,2 +1,2 @@
 #/usr/bin/env bash
-gcalcli --config ~/conky/gcalcliConfig --cal='prueba' --nc --details agenda $(date -d "10 days" +"%m/%d/%Y") $(date -d "31 days" +"%m/%d/%Y")
+gcalcli --config ~/conky/gcalcliConfig --nc --details agenda $(date +"%m/%d/%Y") $(date -d "21 days" +"%m/%d/%Y")
