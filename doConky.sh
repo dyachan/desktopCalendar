19a7abe3692e405361aae0f3c36c0a7e4537911e
dyachan <diego.yachan@gmail.com>
2013-10-10 08:36:13 -0300

    hecho que la actualización en pantalla sea de una y que no se visualicen primero los recordatorios y después todo

--- doConky.sh
+++ doConky.sh
@@ -1,10 +1,16 @@
 #/usr/bin/env bash
+
+## conseguir agenda
+~/conky/getAgenda.sh > ~/conky/tareas
+
+## escribir recordatorios
 cat ~/conky/conky1 > ~/.conkyrc
 cat ~/conky/recordatorios >> ~/.conkyrc
 echo "" >> ~/.conkyrc
 
-~/conky/getAgenda.sh > ~/conky/tareas
+## escribir agenda
 ~/conky/buildAgenda.py ~/conky/tareas >> ~/.conkyrc
 echo "" >> ~/.conkyrc
 
+## eliminar todo rastro de posible búsqueda infiltrada peligrosa y anónima
 rm ~/conky/tareas
