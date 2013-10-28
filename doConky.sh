37901240731228cfe6fd0293ddb488c5c71f5bc1
dyachan <diego.yachan@gmail.com>
2013-10-28 15:59:34 -0300

    agregada opción de actualizar calendario con el último creado. Uso de constantes en archivo doConky.sh

--- doConky.sh
+++ doConky.sh
@@ -1,16 +1,28 @@
 #/usr/bin/env bash
 
+readonly DIR=conky
+readonly OUTPUT=.conkyrc
+
 ## conseguir agenda
-~/conky/getAgenda.sh > ~/conky/tareas
+if [ $# -eq 0 ]
+then
+  $HOME/$DIR/getAgenda.sh > $HOME/$DIR/tareas
+fi
 
 ## escribir recordatorios
-cat ~/conky/conky1 > ~/.conkyrc
-cat ~/conky/recordatorios >> ~/.conkyrc
-echo "" >> ~/.conkyrc
+cat $HOME/$DIR/conky1 > $HOME/$OUTPUT
+cat $HOME/$DIR/recordatorios >> $HOME/$OUTPUT
+echo "" >> $HOME/$OUTPUT
 
 ## escribir agenda
-~/conky/buildAgenda.py ~/conky/tareas >> ~/.conkyrc
-echo "" >> ~/.conkyrc
+if [ $# -eq 0 ]
+then
+  $HOME/$DIR/buildAgenda.py $HOME/$DIR/tareas > $HOME/$DIR/ultimoCal
+  echo "asd"
+fi
+cat $HOME/$DIR/ultimoCal >> $HOME/$OUTPUT
+echo "" >> $HOME/$OUTPUT
 
 ## eliminar todo rastro de posible búsqueda infiltrada peligrosa y anónima
-rm ~/conky/tareas
+rm -f $HOME/$DIR/tareas
+
